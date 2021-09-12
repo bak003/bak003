0330606570729146ac943a2b284577b3bc4a2c09
aaa <aaa@xkx.me>
2021-09-12 18:10:44 +0800

    first commit

---
+++ v2ray/config.sh
@@ -0,0 +1,74 @@
+CONFIG_FILE="/etc/v2ray/config.json"
+PORT=1080
+
+vmessConfig() {
+    local uuid="$(cat '/proc/sys/kernel/random/uuid')"
+    local alterid=`shuf -i50-80 -n1`
+    cat > $CONFIG_FILE<<-EOF
+{
+  "inbounds": [{
+    "port": $PORT,
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "$uuid",
+          "level": 1,
+          "alterId": $alterid
+        }
+      ]
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {}
+  },{
+    "protocol": "blackhole",
+    "settings": {},
+    "tag": "blocked"
+  }]
+}
+EOF
+}
+
+vmessKCPConfig() {
+    local uuid="$(cat '/proc/sys/kernel/random/uuid')"
+    local alterid=`shuf -i50-80 -n1`
+    cat > $CONFIG_FILE<<-EOF
+{
+  "inbounds": [{
+    "port": $PORT,
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "$uuid",
+          "level": 1,
+          "alterId": $alterid
+        }
+      ]
+    },
+    "streamSettings": {
+        "network": "mkcp",
+        "kcpSettings": {
+            "uplinkCapacity": 100,
+            "downlinkCapacity": 100,
+            "congestion": true,
+            "header": {
+                "type": "$HEADER_TYPE"
+            },
+            "seed": "$SEED"
+        }
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {}
+  },{
+    "protocol": "blackhole",
+    "settings": {},
+    "tag": "blocked"
+  }]
+}
+EOF
+}
